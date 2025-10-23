540a9c5d0f5026337b6b40c8cda2df641d433919
nagabcube <ngabor2013@gmail.com>
2025-10-23 11:56:42 +0200

    Refactor Flask app and update database connection
    forked solution

--- flask_app_dataset.py
+++ flask_app_dataset.py
@@ -1,33 +1,17 @@
-from flask import Flask, make_response, jsonify, request
+import os
 import dataset
+from flask import Flask, make_response, jsonify, request
 
+'''
+The most simpliest way...
+'''
 app = Flask(__name__)
-db = dataset.connect('sqlite:///api.db')
-
-'''
-Examples:
-GET request to /api/books returns the details of all books
-POST request to /api/books creates a book with the ID 3 (As per request body)
-
-Sample request body -
-{
-        "book_id": "1",
-        "name": "A Game of Thrones",
-        "author": "George R. R. Martin"
-}
-
-GET request to /api/books/3 returns the details of book 3
-PUT request to /api/books/3 to update fields of book 3
-DELETE request to /api/books/3 deletes book 3
-
-'''
+db = dataset.connect('sqlite:///books.db')
 
 table = db['books']
 
-
-def fetch_db(book_id):  # Each book scnerio
+def fetch_db(book_id):
     return table.find_one(book_id=book_id)
-
 
 def fetch_db_all():
     books = []
@@ -35,21 +19,18 @@ def fetch_db_all():
         books.append(book)
     return books
 
-
 @app.route('/api/db_populate', methods=['GET'])
 def db_populate():
     table.insert({
         "book_id": "1",
-        "name": "A Game of Thrones.",
-        "author": "George R. R. Martin"
+        "name": "The Familiar Spirit",
+        "author": "Butler, D.J. & Ritchey, Aaron Michael"
     })
-
     table.insert({
         "book_id": "2",
-        "name": "Lord of the Rings",
-        "author": "J. R. R. Tolkien"
+        "name": "Futility",
+        "author": "Onoh, Nuzo"
     })
-
     return make_response(jsonify(fetch_db_all()), 200)
 
 @app.get('/')
@@ -70,8 +51,7 @@ def api_books():
         content = request.json
         book_id = content['book_id']
         table.insert(content)
-        return make_response(jsonify(fetch_db(book_id)), 201)  # 201 = Created
-
+        return make_response(jsonify(fetch_db(book_id)), 201)
 
 @app.route('/api/books/<book_id>', methods=['GET', 'PUT', 'DELETE'])
 def api_each_book(book_id):
@@ -81,17 +61,15 @@ def api_each_book(book_id):
             return make_response(jsonify(book_obj), 200)
         else:
             return make_response(jsonify(book_obj), 404)
-    elif request.method == "PUT":  # Updates the book
+    elif request.method == "PUT":
         content = request.json
         table.update(content, ['book_id'])
-
         book_obj = fetch_db(book_id)
         return make_response(jsonify(book_obj), 200)
     elif request.method == "DELETE":
         table.delete(id=book_id)
-
         return make_response(jsonify({}), 204)
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
